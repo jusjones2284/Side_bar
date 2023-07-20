9f4fcae1443cd093bfdb7fe7908efe626139157e
Justin Jones <98709555+jusjones2284@users.noreply.github.com>
2023-07-19 20:46:52 -0400

    created a couple components

---
+++ src/context.jsx
@@ -0,0 +1,13 @@
+import { createContext, useState, useContext } from "react";
+
+const AppContext = createContext()
+
+export const AppProvider = ({ children }) =>{
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSidebar =()=>{
+    setIsSidebarOpen(true);
+  };
+  return <AppContext.Provider>{children} </AppContext.Provider>
+}
