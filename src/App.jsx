fc0acc57b4e184e6451b931256d855c1d130ad13
Justin Jones <98709555+jusjones2284@users.noreply.github.com>
2023-07-20 20:11:41 -0400

    finishing up project

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ import React from 'react'
 const App = () => {
   return (
     <>
-      
+      <Home />
       <Modal />
       <Sidebar /> 
-      <Home />
+      
    
 
       
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -7,7 +7,7 @@ const Sidebar = () => {
   const { isSidebarOpen, closeSidebar } = useGlobalContext();
 
   return (
-   <aside className='sidebar'>
+   <aside className={isSidebarOpen ? 'sidebar show-sidebar': 'sidebar'}>
     <div className="sidebar-header">
         <img src={logo} alt="coding addict" className='logo' />
         <button className='close-btn' onClick={closeSidebar}>
@@ -20,7 +20,7 @@ const Sidebar = () => {
         return <li key={id}>
           <a href={url}>
             {icon}
-            {text}
+            {text} 
           </a>
         </li>
       })}
